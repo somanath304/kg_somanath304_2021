729c1fe4736fb7bf2a4b91523a9d4d42a3efe05e
Somanath Joglekar <som22@appleadmins-MacBook-Pro-2.local>
2020-03-20 00:14:53 -0400

    comments added

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ def mapping(s1,s2):
         print("false")
         return
 
-    s1 = set(s1)
+    s1 = set(s1) #the strings are converted into a set to remove duplicates
     s2 = set(s2)
     # print(s1,s2)
-    if len(s1)<len(s2):
+    if len(s1)<len(s2): # comparing the length of sets
         print("false")
     else :
         print("true")
@@ -16,8 +16,8 @@ def mapping(s1,s2):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('value',nargs='+',help='an integer for the accumulator')
+    parser = argparse.ArgumentParser(description='Passed values taken as input')
+    parser.add_argument('value',nargs='+')
 
     args = parser.parse_args()
     mapping(args.value[0],args.value[1])
